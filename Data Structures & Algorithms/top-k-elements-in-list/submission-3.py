d96bb5caa454ea9b5cb6eeb5ece057c98b494ae2
bindingsoul <85997639+bindingsoul@users.noreply.github.com>
2026-08-26 06:45:36 +0530

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freqmap = {}
+        bucket = [[] for x in range(len(nums)+1)]
+
+        for x in range(len(nums)):
+            freqmap[nums[x]]=1+freqmap.get(nums[x],0)
+        
+        for key, val in freqmap.items():
+            bucket[val].append(key)
+
+        res = []
+
+        for x in range(len(bucket)-1,0,-1):
+            if len(res)==k:
+                return res
+            if len(bucket[x])!=0:
+                res.extend(bucket[x])
+
+            
+            
