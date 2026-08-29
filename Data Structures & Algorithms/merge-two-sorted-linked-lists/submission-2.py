f7e807b0cbda4a41faf6c4a7538161b1ce099ab7
bindingsoul <85997639+bindingsoul@users.noreply.github.com>
2026-08-29 21:15:22 +0530

    Add: merge-two-sorted-linked-lists - submission-2

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-2.py
@@ -0,0 +1,82 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def count(self, list1: Optional[ListNode]) -> int:
+        ans = 0
+        curr = list1
+        while curr:
+            curr = curr.next
+            ans+=1
+        return ans
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        l1 = self.count(list1)
+        l2 = self.count(list2)
+        res = ListNode()
+        head = res
+        i = list1
+        j = list2
+        while l1 and l2:
+            if i.val==j.val:
+                res.next = ListNode(i.val)
+                res = res.next
+                res.next = ListNode(j.val)
+                res = res.next
+                i = i.next
+                j = j.next
+                l1-=1
+                l2-=1
+            if i.val<j.val:
+                res.next = ListNode(i.val)
+                res = res.next
+                i=i.next
+                l1-=1
+            else:
+                res.next = ListNode(j.val)
+                res = res.next
+                j=j.next
+                l2-=1
+
+        if l1==l2:
+            return res
+        if l1:
+            while(i):
+                res.next = ListNode(i.val)
+                res = res.next
+                i = i.next
+        if l2:
+            while(j):
+                res.next = ListNode(j.val)
+                res = res.next
+                j = j.next
+
+        return head
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
